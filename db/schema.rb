4a5b423551f8d87edee13528e4c4c791c5836f21
Ric-Lavers <ric_lavers@outlook.com>
2017-09-30 20:14:49 +1000

    ..

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20170929051229) do
-=======
-ActiveRecord::Schema.define(version: 20170929034908) do
->>>>>>> master
 
   create_table "tweets", force: :cascade do |t|
     t.string "message"
