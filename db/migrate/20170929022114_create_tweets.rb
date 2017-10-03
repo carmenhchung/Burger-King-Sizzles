6461e49853ac3da0d407f812426d7fddd5ecfe7f
Young Jae Nam <Jaenam@youngjaenamsair.ait-sydney.net.au>
2017-10-03 13:45:44 +1100

    db

--- db/migrate/20170929022114_create_tweets.rb
+++ db/migrate/20170929022114_create_tweets.rb
@@ -5,6 +5,5 @@ class CreateTweets < ActiveRecord::Migration[5.1]
       t.timestamps
       t.references :user, foreign_key: true
     end
-    add_index :microposts, [:user_id, :created_at]
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171002203939) do
+ActiveRecord::Schema.define(version: 20171001014119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "comments", force: :cascade do |t|
-    t.string "name"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "crowns", force: :cascade do |t|
     t.integer "tweet_id"
@@ -41,27 +34,10 @@ ActiveRecord::Schema.define(version: 20171002203939) do
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.string "avatar"
-    t.integer "user_id"
     t.string "retweet"
-    t.integer "comment_id"
-  end
-
-  create_table "user_names", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet "current_sign_in_ip"
-    t.inet "last_sign_in_ip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_user_names_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_user_names_on_reset_password_token", unique: true
+    t.index ["user_id"], name: "index_tweets_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -82,6 +58,7 @@ ActiveRecord::Schema.define(version: 20171002203939) do
     t.string "interests"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["user_name"], name: "index_users_on_user_name", unique: true
   end
 
   create_table "votes", id: :serial, force: :cascade do |t|
@@ -100,4 +77,5 @@ ActiveRecord::Schema.define(version: 20171002203939) do
     t.index ["voter_type", "voter_id"], name: "index_votes_on_voter_type_and_voter_id"
   end
 
+  add_foreign_key "tweets", "users"
 end
