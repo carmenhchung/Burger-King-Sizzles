2e5d9ead2f8a201567b54ff196622c8f497b7908
KasumiTakeshima <kasumi0201@gmail.com>
2017-10-03 12:51:52 +1100

    added controllers and helpers

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,17 @@
+# class HomeController < ApplicationController
+#   def index
+#     @brand = "Twitter"
+#     @tweets = Tweet.last(20)
+#   end
+#
+# end
+
 class HomeController < ApplicationController
   def index
-    @brand = "Twitter"
+    @brand = "Burger King Sizzle"
     @tweets = Tweet.last(20)
   end
-
+  def profile
+    @User = User.find(params[:id])
+  end
 end
